74644706fa4b3c1ff681eca58a82e8bb0b8619cf
tpschock <tschock23@gmail.com>
2026-07-16 07:57:23 -0500

    Create BrandHeader.tsx

---
+++ components/BrandHeader.tsx
@@ -0,0 +1,9 @@
+import FocusLogo from "./FocusLogo";
+
+export default function BrandHeader() {
+  return (
+    <div className="sticky top-0 z-10 border-b border-slate-200 bg-white px-4 py-2.5">
+      <FocusLogo />
+    </div>
+  );
+}
